1537b1778b8d80f84a7fd62861967f25aa065b09
Thomas Hunger <tehunger@gmail.com>
2014-03-02 23:02:49 +0000

    Make BoxHelper work again.

--- lib/extras/helpers/box_helper.dart
+++ lib/extras/helpers/box_helper.dart
@@ -1,7 +1,7 @@
 part of three;
 
 class BoxHelper extends Line {
-  List<Vector3> vertices = [new Vector3( 1.0,  1.0,  1.0),
+  static List<Vector3> vertices = [new Vector3( 1.0,  1.0,  1.0),
                             new Vector3(-1.0,  1.0,  1.0),
                             new Vector3(-1.0, -1.0,  1.0),
                             new Vector3( 1.0, -1.0,  1.0),
@@ -11,9 +11,9 @@ class BoxHelper extends Line {
                             new Vector3(-1.0, -1.0, -1.0),
                             new Vector3( 1.0, -1.0, -1.0)];
   
-  factory BoxHelper(Mesh object) {
+  factory BoxHelper([Mesh object]) {
     var geometry = new Geometry()
-    ..vertices.addAll([vertices[0], vertices[1],
+      ..vertices.addAll([vertices[0], vertices[1],
                        vertices[1], vertices[2],
                        vertices[2], vertices[3],
                        vertices[3], vertices[0],
@@ -34,7 +34,7 @@ class BoxHelper extends Line {
   
   BoxHelper._internal(Geometry geometry, Material material, int type) : super(geometry, material, type);
   
-  update(Mesh object) {
+  static update(Mesh object) {
     var geometry = object.geometry;
 
     if (geometry.boundingBox == null) {
